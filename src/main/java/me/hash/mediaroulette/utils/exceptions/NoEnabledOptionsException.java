4a131d1fac29a9bdcdcf394535a6170fe9457ff9
Hashyies <shaddiyar2@gmail.com>
2024-09-16 00:20:14 +0300

    Fixing bugs

--- src/main/java/me/hash/mediaroulette/utils/exceptions/NoEnabledOptionsException.java
+++ src/main/java/me/hash/mediaroulette/utils/exceptions/NoEnabledOptionsException.java
@@ -3,5 +3,5 @@ package me.hash.mediaroulette.utils.exceptions;
 public class NoEnabledOptionsException extends Exception {
     public NoEnabledOptionsException(String message) {
         super(message);
-    }
+    } // pull request test
 }
